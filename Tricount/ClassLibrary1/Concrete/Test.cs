15193cfe51f96e5e77d1e65a8f31f2468ded8ba5
Ege Konhan <129951796+egekonhan@users.noreply.github.com>
2023-08-16 14:17:53 +0300

    Entity katmaninda base entyity eklendi

---
+++ Tricount/ClassLibrary1/Concrete/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriCount.Entites.Abstract;
+
+namespace TriCount.Entites.Concrete
+{
+    public class Test:BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
