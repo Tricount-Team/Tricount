f3cb31b8d2fad970adfc354e57827b6868cceabf
Ege Konhan <129951796+egekonhan@users.noreply.github.com>
2023-08-16 15:43:07 +0300

    Expense yeni

---
+++ Tricount/Tricount.Entities/Concrete/Expense.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tricount.Entities.Abstract;
+
+namespace Tricount.Entities.Concrete
+{
+    public class Expense : BaseEntity
+    {
+        public int Description { get; set; }
+
+        public int Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        public int GroupId { get; set; }
+
+        public Group Group { get; set; }
+    }
+}
