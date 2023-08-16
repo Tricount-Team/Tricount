7dcc511871de543db6de251224b1151f2f0359ec
Dılşad Yağmur <dilsadygmur@gmail.com>
2023-08-17 00:19:58 +0300

    repository yazıldı

---
+++ Tricount/Tricount.DAL/Repositories/Abstract/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Tricount.Entities.Abstract;
+
+namespace Tricount.DAL.Repositories.Abstract
+{
+	public interface IBaseRepository<T> where T : BaseEntity, new()
+	{
+		Task<int> Insert(T input);
+		Task<int> Update(T input);
+		Task<int> Delete(T input);
+
+		Task<T?> GetById(int id);
+
+		Task<ICollection<T>>? GetAll(Expression<Func<T, bool>>? filter =null );
+		
+		Task<IQueryable<T>>? GetAllInclude(Expression<Func<T, bool>>? filter=null, params Expression<Func<T, object>>[]? include);
+
+	}
+}
