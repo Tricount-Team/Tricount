5ebe76732af87ecc9e06382696d2baad826ad4a6
Dılşad Yağmur <dilsadygmur@gmail.com>
2023-08-17 02:04:02 +0300

    Extensions yazıldı

---
+++ Tricount/Tricount.MVC/Extensions/TricountManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Tricount.BL.Abstract;
+using Tricount.BL.Concrete;
+using Tricount.DAL.Repositories.Abstract;
+using Tricount.DAL.Repositories.Concrete;
+using IGroupManager = Tricount.BL.Abstract.IGroupManager;
+
+namespace Tricount.MVC.Extensions
+{
+	public static class TricountManager
+	{
+		public static IServiceCollection AddTricountServices(this IServiceCollection services)
+		{
+			services.AddScoped<IExpenseRepository, ExpenseRepository>();
+			services.AddScoped<IExpenseDetailRepository, ExpenseDetailRepository>();
+			services.AddScoped<IPaymentRepository, PaymentRepository>();
+			services.AddScoped<IGroupRepository, GroupRepository>();
+			services.AddScoped<IExpenseManager, ExpenseManager>();
+			services.AddScoped<IGroupManager, GroupManager>();
+			services.AddScoped<IPaymentManager, PaymentManager>();
+			services.AddScoped<IExpenseDetailManager, ExpenseDetailManager>();
+
+
+
+			return services;
+		}
+	}
+}
