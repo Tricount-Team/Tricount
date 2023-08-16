e6d3ce99f97cdc2105b70a042938d27d093ede02
Dılşad Yağmur <dilsadygmur@gmail.com>
2023-08-17 01:13:14 +0300

    dto

--- Tricount/Tricount.MVC/Models/DTO's/PaymentCreateDTO.cs
+++ Tricount/Tricount.MVC/Models/DTO's/PaymentCreateDTO.cs
@@ -1,6 +1,10 @@
-﻿namespace Tricount.MVC.Models.DTO_s
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tricount.MVC.Models.DTO_s
 {
 	public class PaymentCreateDTO
 	{
-	}
+		[Required(AllowEmptyStrings = false, ErrorMessage ="Ücret kısmı boş geçilemez.")]
+        public double Amount { get; set; }
+    }
 }
