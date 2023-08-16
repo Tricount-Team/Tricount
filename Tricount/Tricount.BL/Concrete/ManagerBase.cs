ea6429d7ff7c4da7b7ef350fc96ea149aa49ff9d
Ege Konhan <129951796+egekonhan@users.noreply.github.com>
2023-08-17 01:40:18 +0300

    AutoMapper Yapildi

--- Tricount/Tricount.BL/Concrete/ManagerBase.cs
+++ Tricount/Tricount.BL/Concrete/ManagerBase.cs
@@ -13,28 +13,11 @@ namespace Tricount.BL.Concrete
     public class ManagerBase<T> : IManagerBase<T> where T : BaseEntity, new()
     {
         private readonly IBaseRepository<T> repository;
-        private IPaymentRepository repository1;
-        private IGroupRepository repository2;
-        private IExpenseDetailRepository repository3;
+        
 
         public ManagerBase(IBaseRepository<T> repository)
         {
             this.repository = repository;
-        }
-
-        public ManagerBase(IPaymentRepository repository1)
-        {
-            this.repository1 = repository1;
-        }
-
-        public ManagerBase(IGroupRepository repository2)
-        {
-            this.repository2 = repository2;
-        }
-
-        public ManagerBase(IExpenseDetailRepository repository3)
-        {
-            this.repository3 = repository3;
         }
 
         public async virtual Task<int> Insert(T entity)
@@ -64,3 +47,4 @@ namespace Tricount.BL.Concrete
             return await repository.GetAllInclude(filter, include);
         }
     }
+}
