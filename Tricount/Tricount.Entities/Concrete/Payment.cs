996a8f36bbac35154167ff0224afe6b8831000b0
Dılşad Yağmur <dilsadygmur@gmail.com>
2023-08-16 15:35:56 +0300

    Group ve Payment classları eklendi

---
+++ Tricount/Tricount.Entities/Concrete/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tricount.Entities.Abstract;
+
+namespace Tricount.Entities.Concrete
+{
+    public class Payment : BaseEntity
+    {
+        public double Amount { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ExpenseId { get; set; }
+        public Expense Expense { get; set; }
+    }
+}
