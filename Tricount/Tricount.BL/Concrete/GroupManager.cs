dde5ade5043c15ac301b5c6c25ac9561673dc331
Ege Konhan <129951796+egekonhan@users.noreply.github.com>
2023-08-23 15:40:48 +0300

    22

---
+++ Tricount/Tricount.BL/Concrete/GroupManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tricount.BL.Abstract;
+using Tricount.DAL.Repositories.Abstract;
+using Tricount.Entities.Concrete;
+
+namespace Tricount.BL.Concrete
+{
+    public class GroupManager : ManagerBase<Group>, IGroupManager
+    {
+        public GroupManager(IGroupRepository repository) : base(repository)
+        {
+
+        }
+    }
+}
+
