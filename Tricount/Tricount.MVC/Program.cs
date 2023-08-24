a9bebbd5d575d39d7da6ad0c88805a5ca38400e1
Dılşad Yağmur <dilsadygmur@gmail.com>
2023-08-24 13:10:37 +0300

    identitiy eklendi

--- Tricount/Tricount.MVC/Program.cs
+++ Tricount/Tricount.MVC/Program.cs
@@ -13,7 +13,7 @@ namespace Tricount.MVC
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString(@"Server=(localdb)\mssqllocaldb;Database=Tricount1;Trusted_Connection=True;");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
